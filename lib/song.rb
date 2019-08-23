22142070f8003305e7df86f064945660406fa444
Matt Pitner <mattpitner09@gmail.com>
2019-08-23 15:06:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  
+   attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artists << self 
+    @name = name 
+    @artist = artist
+    @genre = genre 
+  end 
+  
+  def self.count 
+    @@count
+  end 
+  
+  def genres 
+    @@genres 
+  end 
+  
+  def self.artists
+   @@artists
+  end 
+  
+  def self.genre_count 
+    
+  end 
+  
+ 
+  
+ 
+  
+  
+end 
