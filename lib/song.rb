2847d0c7e9c4a7e70750ed5d94e2a0bc515d73b9
Matt Pitner <mattpitner09@gmail.com>
2019-08-23 15:11:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   
   def initialize(name, artist, genre)
     @@count += 1
-    @@artists << artists
+    @@artists << artist
     @@genres << genres
     @name = name 
     @artist = artist
